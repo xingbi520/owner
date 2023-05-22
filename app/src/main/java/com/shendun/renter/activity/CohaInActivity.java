8b740577ce3056b0cacce96be6c01021b4fbe333
xingbi520 <xingbi520@163.com>
2023-05-22 18:01:28 +0800

    同住人移除选择相册。修改文字等

--- app/src/main/java/com/shendun/renter/activity/CohaInActivity.java
+++ app/src/main/java/com/shendun/renter/activity/CohaInActivity.java
@@ -746,9 +746,9 @@ public class CohaInActivity extends BaseActivity<ActivityAddCohabitantBinding>
         MenuItem menuItem1 = new MenuItem();
         menuItem1.setText(getString(R.string.photo_from_camera));
         menuItem1.setStyle(MenuItem.MenuItemStyle.COMMON);
-        MenuItem menuItem2 = new MenuItem();
-        menuItem2.setText(getString(R.string.photo_from_album));
-        menuItem2.setStyle(MenuItem.MenuItemStyle.COMMON);
+//        MenuItem menuItem2 = new MenuItem();
+//        menuItem2.setText(getString(R.string.photo_from_album));
+//        menuItem2.setStyle(MenuItem.MenuItemStyle.COMMON);
         menuItem1.setMenuItemOnClickListener(
                 new MenuItemOnClickListener(bottomMenuFragment, menuItem1) {
                     @Override
@@ -756,15 +756,15 @@ public class CohaInActivity extends BaseActivity<ActivityAddCohabitantBinding>
                         takePhotoClick();
                     }
                 });
-        menuItem2.setMenuItemOnClickListener(
-                new MenuItemOnClickListener(bottomMenuFragment, menuItem2) {
-                    @Override
-                    public void onClickMenuItem(android.view.View v, MenuItem menuItem) {
-                        openAlbumClick();
-                    }
-                });
+//        menuItem2.setMenuItemOnClickListener(
+//                new MenuItemOnClickListener(bottomMenuFragment, menuItem2) {
+//                    @Override
+//                    public void onClickMenuItem(android.view.View v, MenuItem menuItem) {
+//                        openAlbumClick();
+//                    }
+//                });
         menuItemList.add(menuItem1);
-        menuItemList.add(menuItem2);
+//        menuItemList.add(menuItem2);
         bottomMenuFragment.setMenuItems(menuItemList);
         bottomMenuFragment.show(getSupportFragmentManager(), "BottomMenuFragment");
     }
